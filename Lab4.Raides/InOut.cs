b74a914278412632be52a0ee096f1e22864afec7
Rokas Puzonas <rokas.puz@gmail.com>
2021-11-17 01:38:49 +0200

    feat: initial commit

---
+++ Lab4.Raides/InOut.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Lab4.Raides
+{
+    static class InOut
+    {
+        /** Prints repetition of letters using two columns into a given file.
+        @param fout – name of the file for the output
+        @param letters – object having letters and their repetitions */
+        public static void PrintRepetitions(string fout, LettersFrequency letters)
+        {
+            using (var writer = File.CreateText(fout))
+            {
+                foreach (var pair in letters.GetOrderedFrequencies())
+                {
+                    writer.WriteLine("{0, 3:c} {1, 4:d}", pair.Item1, pair.Item2);
+                }
+            }
+        }
+
+        /** Inputs from the given data file and counts repetition of letters.
+         @param fin – name of data file
+         @param letters – object having letters and their repetitions*/
+        public static void Repetitions(string fin, LettersFrequency letters)
+        {
+            using (StreamReader reader = new StreamReader(fin))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    letters.line = line;
+                    letters.CountLetters();
+                }
+            }
+        }
+    }
+}
---
+++ Lab4.Raides/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab4.Raides
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string CFd = "Duomenys.txt";
+            const string CFr = "Rezultatai.txt";
+            LettersFrequency letters = new LettersFrequency();
+            InOut.Repetitions(CFd, letters);
+            char mostFrequent = letters.GetMostFrequent();
+            if (mostFrequent != 0)
+            {
+                Console.WriteLine("Dažniausiai pasikartojanti raidė: {0}", mostFrequent);
+            }
+            InOut.PrintRepetitions(CFr, letters);
+        }
+    }
+}
