3ca49e564a342f88b74553b6241ff88bb3ccf006
Rokas Puzonas <rokas.puz@gmail.com>
2021-11-22 01:26:55 +0200

    feat: add "Lab4.FirstEqualLast" using strings

---
+++ Lab4.FirstEqualLast/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab4.FirstEqualLast
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			const string CFd = "Duomenys.txt";
+			char [] punctuation = {' ','.',',','!','?',':',';','(',')','\t'};
+			Console.WriteLine("Sutampančių žodžių {0, 3:d}", TaskUtils.Process(CFd, punctuation));
+		}
+	}
+}
---
+++ Lab4.FirstEqualLast/TaskUtils.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab4.FirstEqualLast
+{
+	public class TaskUtils
+	{
+		/** Reads file and finds the number of words having same the first and
+			the last letters.
+			@param fin – name of data file
+			@param punctuation – punctuation marks to separate words */
+		public static int Process(string fin, char[] punctuation)
+		{
+			string[] lines = File.ReadAllLines(fin, Encoding.UTF8);
+			int equal = 0;
+			foreach (string line in lines)
+			if (line.Length > 0)
+			equal += FirstEqualLast(line, punctuation);
+			return equal;
+		}
+
+		/** Splits line into words and counts the words having same the first and the
+			last letters.
+			@param line – string of data
+			@param punctuation – punctuation marks to separate words */
+		private static int FirstEqualLast (string line, char[] punctuation)
+		{
+			string[] parts = line.Split(punctuation,
+			StringSplitOptions.RemoveEmptyEntries);
+			int equal = 0;
+			foreach (string word in parts)
+				if (word[0] == word[word.Length - 1])
+					equal++;
+			return equal;
+		}
+	}
+}
